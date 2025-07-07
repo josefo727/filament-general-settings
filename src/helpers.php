2bcf1b53bf5dc18f27cc0096046525ba5ad6a04d
José R. Gutierrez <josefo727@gmail.com>
2025-07-06 21:00:16 -0500

    refactor: Simplify `getSetting` helper by delegating default handling to `getValue` method

--- src/helpers.php
+++ src/helpers.php
@@ -8,7 +8,6 @@ if (!function_exists('getSetting')) {
      */
     function getSetting(string $key, mixed $default = null): mixed
     {
-        $value = \Josefo727\FilamentGeneralSettings\Models\GeneralSetting::getValue($key);
-        return $value ?? $default;
+        return \Josefo727\FilamentGeneralSettings\Models\GeneralSetting::getValue($key, $default);
     }
 }
